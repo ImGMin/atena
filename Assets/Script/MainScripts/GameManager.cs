5ac573eb1080e664f11fd63492d5a57ba7b57325
klmchljeon <sl1slq96@gmail.com>
2024-06-14 05:15:12 +0900

    situ list stabilization

--- Assets/Script/MainScripts/GameManager.cs
+++ Assets/Script/MainScripts/GameManager.cs
@@ -47,6 +47,7 @@ public class GameManager : MonoBehaviour
             DontDestroyOnLoad(gameObject); 
             dataPath = Path.Combine(Application.persistentDataPath, "gameData.json");
             LoadGameData();
+            GenSitu();
         }
         else if (instance != this)
         {
@@ -68,6 +69,60 @@ public class GameManager : MonoBehaviour
         };
     }
 
+    private void GenSitu()
+    {
+        List<Dictionary<string, string>> data = new List<Dictionary<string, string>>();
+
+        TextAsset csvData = Resources.Load<TextAsset>("test_date");
+        // 파일 경로가 유효한지 확인
+        if (csvData != null)
+        {
+            // CSV 파일의 모든 줄을 읽기
+            string[] lines = csvData.text.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+
+            if (lines.Length > 0)
+            {
+                // 첫 번째 줄은 헤더로 사용
+                string[] headers = lines[0].Split(',');
+
+                // 각 행의 데이터를 딕셔너리에 저장
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    if (!string.IsNullOrEmpty(lines[i]))
+                    {
+                        string[] fields = lines[i].Split(',');
+
+                        Dictionary<string, string> entry = new Dictionary<string, string>();
+
+                        for (int j = 0; j < headers.Length; j++)
+                        {
+                            entry[headers[j]] = fields[j];
+                        }
+
+                        data.Add(entry);
+                    }
+                }
+            }
+        }
+        else
+        {
+            Debug.LogError("CSV 파일을 찾을 수 없습니다");
+            return;
+        }
+
+        for (int i = 0; i < data.Count; i++)
+        {
+            string str = data[i]["상황ID"];
+
+            if (!Instance.SituToNum.ContainsKey(str))
+            {
+                Instance.SituToNum[str] = Instance.SituToNum.Count;
+                Instance.NumToSitu.Add(str);
+            }
+
+        }
+    }
+
     public void SaveGameData()
     {
         string jsonData = JsonConvert.SerializeObject(gameData, Formatting.Indented);
@@ -85,6 +140,8 @@ public class GameManager : MonoBehaviour
         {
             gameData = new GameData();
         }
+
+
     }
 
     public void InitGameData()
