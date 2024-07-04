9b31d820d96d098b25623eb877c5f1e03fc1059d
klmchljeon <sl1slq96@gamil.com>
2024-07-04 17:57:22 +0900

    popup image alpha

--- Assets/Script/TestScripts/ImagePopup.cs
+++ Assets/Script/TestScripts/ImagePopup.cs
@@ -1,12 +1,19 @@
-using Microsoft.Unity.VisualStudio.Editor;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ImagePopup : MonoBehaviour
 {
     public float time = 0f;
     public float speed = 0.1f;
+
+    private Image image;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -19,7 +26,11 @@ public class ImagePopup : MonoBehaviour
             transform.position.z
         );
 
-        if (time >= 2.0)
+        Color color = image.color;
+        color.a = Mathf.Clamp01(1 - time/2);
+        image.color = color;
+
+        if (time >= 2.0f)
         {
             Destroy(gameObject);
             Debug.Log("asdfasdfasdf");
