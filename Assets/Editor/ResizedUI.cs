a124b36784409932244175a46a1b84117006e402
klmchljeon <sl1slq96@gamil.com>
2024-07-29 15:39:51 +0900

    resize UI 2

--- Assets/Editor/ResizedUI.cs
+++ Assets/Editor/ResizedUI.cs
@@ -32,9 +32,107 @@ public class ResizeUIEditor : EditorWindow
 
             ChangeTextAsset();
         }
+        if (GUILayout.Button("Func"))
+        {
+
+            Func();
+        }
+        if (GUILayout.Button("FuncRev"))
+        {
+
+            FuncRev();
+        }
     }
+    void FuncRev()
+    {
+        GameObject[] allGameObjects = Resources.FindObjectsOfTypeAll<GameObject>();
+
+        foreach (GameObject go in allGameObjects)
+        {
+            if (go.activeSelf)
+                continue;
+
+            RectTransform rectTransform = go.GetComponent<RectTransform>();
+            if (rectTransform != null)
+            {
+                // 현재 크기를 가져옵니다.
+                Vector2 currentSize = rectTransform.sizeDelta;
+
+                // 크기를 2배로 설정합니다.
+                rectTransform.sizeDelta = currentSize / 2;
+                rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x, rectTransform.anchoredPosition.y) / 2;
+
+                // 변경 사항을 저장합니다.
+                EditorUtility.SetDirty(rectTransform);
+            }
+
+            TextMeshProUGUI tmpText = go.GetComponent<TextMeshProUGUI>();
+            if (tmpText != null)
+            {
+                // 현재 폰트 크기를 가져옵니다.
+                float currentFontSize = tmpText.fontSize;
+
+                // 폰트 크기를 2배로 설정합니다.
+                tmpText.fontSize = currentFontSize / 2;
+                //tmpText.rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x, rectTransform.anchoredPosition.y);
+                tmpText.font = newFont;
+
+                // 변경 사항을 저장합니다.
+                EditorUtility.SetDirty(tmpText);
+            }
 
 
+        }
+
+        // 변경 사항을 저장합니다.
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+    void Func()
+    {
+        GameObject[] allGameObjects = Resources.FindObjectsOfTypeAll<GameObject>();
+
+        foreach (GameObject go in allGameObjects)
+        {
+            if (go.activeSelf)
+                continue;
+
+            RectTransform rectTransform = go.GetComponent<RectTransform>();
+            if (rectTransform != null)
+            {
+                // 현재 크기를 가져옵니다.
+                Vector2 currentSize = rectTransform.sizeDelta;
+
+                // 크기를 2배로 설정합니다.
+                rectTransform.sizeDelta = currentSize * 2;
+                rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x, rectTransform.anchoredPosition.y) * 2;
+
+                // 변경 사항을 저장합니다.
+                EditorUtility.SetDirty(rectTransform);
+            }
+
+            TextMeshProUGUI tmpText = go.GetComponent<TextMeshProUGUI>();
+            if (tmpText != null)
+            {
+                // 현재 폰트 크기를 가져옵니다.
+                float currentFontSize = tmpText.fontSize;
+
+                // 폰트 크기를 2배로 설정합니다.
+                tmpText.fontSize = currentFontSize * 2;
+                //tmpText.rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x, rectTransform.anchoredPosition.y);
+                tmpText.font = newFont;
+
+                // 변경 사항을 저장합니다.
+                EditorUtility.SetDirty(tmpText);
+            }
+
+
+        }
+
+        // 변경 사항을 저장합니다.
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
 
     void ResizeAllUI()
     {
