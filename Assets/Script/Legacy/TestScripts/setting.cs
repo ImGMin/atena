96c6fd38363588128c9ed1b8ee857081105ef904
kith8146 <kith8146@gmail.com>
2024-11-18 20:04:24 +0900

    음량설정, 폰트설정

--- Assets/Script/Legacy/TestScripts/setting.cs
+++ Assets/Script/Legacy/TestScripts/setting.cs
@@ -3,4 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public class setting : MonoBehaviour
+{
+    public Slider volumeSlider;
+    public Slider bgmVolumeSlider;
+    public Slider effectVolumeSlider;
 
+    private void Start()
+    {
+        Slider[] allSlider = Resources.FindObjectsOfTypeAll<Slider>();
+
+
+        volumeSlider.value = SoundManager.Instance.Volume;
+        bgmVolumeSlider.value = SoundManager.Instance.bgmVolume;
+        effectVolumeSlider.value = SoundManager.Instance.effectVolume;
+
+
+        volumeSlider.onValueChanged.AddListener(SetVolume);
+        bgmVolumeSlider.onValueChanged.AddListener(SetbgmVolume);
+        effectVolumeSlider.onValueChanged.AddListener(SeteffectVolume);
+    }
+
+    public void SetVolume(float value)
+    {
+        SoundManager.Instance.SetVolume(value);
+    }
+
+    public void SetbgmVolume(float value)
+    {
+        SoundManager.Instance.SetbgmVolume(value);
+    }
+
+    public void SeteffectVolume(float value)
+    {
+        SoundManager.Instance.SeteffectVolume(value);
+    }
+}
